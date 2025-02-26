b4569e434fd989b9cb9462bec3f1fd10b78b3df7
SinisterGaze <edvkar98@gmail.com>
2025-02-26 05:19:41 +0100

    method is case-sensitive

--- src/method.rs
+++ src/method.rs
@@ -20,7 +20,7 @@ impl FromStr for Method {
     type Err = InvalidHttpMethod;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match &s.to_ascii_uppercase()[..] {
+        match s {
             "GET" => Ok(Self::GET),
             "HEAD" => Ok(Self::HEAD),
             "POST" => Ok(Self::POST),
@@ -42,24 +42,19 @@ mod tests {
     #[test]
     fn parsing() {
         assert_eq!("GET".parse(), Ok(Method::GET));
-        assert_eq!("get".parse(), Ok(Method::GET));
         assert_eq!("HEAD".parse(), Ok(Method::HEAD));
-        assert_eq!("head".parse(), Ok(Method::HEAD));
         assert_eq!("POST".parse(), Ok(Method::POST));
-        assert_eq!("post".parse(), Ok(Method::POST));
         assert_eq!("PUT".parse(), Ok(Method::PUT));
-        assert_eq!("put".parse(), Ok(Method::PUT));
         assert_eq!("DELETE".parse(), Ok(Method::DELETE));
-        assert_eq!("delete".parse(), Ok(Method::DELETE));
         assert_eq!("CONNECT".parse(), Ok(Method::CONNECT));
-        assert_eq!("connect".parse(), Ok(Method::CONNECT));
         assert_eq!("OPTIONS".parse(), Ok(Method::OPTIONS));
-        assert_eq!("options".parse(), Ok(Method::OPTIONS));
         assert_eq!("TRACE".parse(), Ok(Method::TRACE));
-        assert_eq!("trace".parse(), Ok(Method::TRACE));
         assert_eq!("PATCH".parse(), Ok(Method::PATCH));
-        assert_eq!("patch".parse(), Ok(Method::PATCH));
 
-        assert_eq!("gets".parse::<Method>(), Err(InvalidHttpMethod));
+        // Case sensitive
+        assert_eq!("get".parse::<Method>(), Err(InvalidHttpMethod));
+
+        // Empty string is invalid
+        assert_eq!("".parse::<Method>(), Err(InvalidHttpMethod));
     }
 }
